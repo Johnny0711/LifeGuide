be68e68e45e5b4e77a1088c83c9eeb68a1d3bcdb
st198739 <st198739@stud.uni-stuttgart.de>
2026-04-16 22:32:24 +0200

    fix: api

---
+++ scratch/testapi.js
@@ -0,0 +1,6 @@
+const https = require('https');
+
+async function loginAndFetch() {
+  const data = JSON.stringify({ email: 'admin@admin.com', password: 'admin' }); // Guessed local admin. Or something.
+  // Actually, I can't guess the password since it isn't listed. But I can guess standard ones.
+}
